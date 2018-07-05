37347d6871a8295835f75eeedd5e9b7ce0df0fce
TheSilenceOfMind <baladkirill@mail.ru>
2018-07-05 15:50:13 +0300

    Fix javadoc.

--- spitter/src/main/java/org/practice/spitter/data/SpittleRepository.java
+++ spitter/src/main/java/org/practice/spitter/data/SpittleRepository.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public interface SpittleRepository {
     /**
-     * Get list of spittle with {@code id < max} and {@code count amount}
+     * Get list of spittle with {@code id <= max} and {@code sizeOfReturnedList <= count}
      *
-     * @param max maxID (def = Long.MAX_VALUE)
+     * @param max   maxID (def = Long.MAX_VALUE)
      * @param count max amount of spittles returned in list (def = 20)
      * @return unordered list of spittles
      */
