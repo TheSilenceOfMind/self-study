2592d478948c3b3c06e2447c220ca390912cd3ac
TheSilenceOfMind <baladkirill@mail.ru>
2018-07-05 16:07:52 +0300

    Add test for parametrized request and refactor class name.

--- spitter/src/test/org/practice/spitter/web/ControllerTests.java
+++ spitter/src/test/org/practice/spitter/web/ControllerTests.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class HomeControllerTest {
+public class ControllerTests {
     @Test
     public void testHomePage() throws Exception {
         HomeController controller = new HomeController();
@@ -42,6 +42,24 @@ public class HomeControllerTest {
                 .andExpect(view().name("spittles"))
                 .andExpect(model().attributeExists("spittleList"))
                 .andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+    }
+
+    @Test
+    public void shouldShowPagedSpittles() throws Exception {
+        List<Spittle> expectedList = createSpittlesList(50);
+        SpittleRepository mockRepository = mock(SpittleRepository.class);
+        when(mockRepository.findSpittles(238_900, 50))
+                .thenReturn(expectedList);
+
+        SpittleController controller = new SpittleController(mockRepository);
+        MockMvc mockMvc = standaloneSetup(controller)
+                .setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+                .build();
+
+        mockMvc.perform(get("/spittles?max=238900&count=50"))
+                .andExpect(view().name("spittles"))
+                .andExpect(model().attributeExists("spittleList"))
+                .andExpect(model().attribute("spittleList", hasItems(expectedList.toArray())));
     }
 
     private List<Spittle> createSpittlesList(int count) {
