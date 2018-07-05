fc9e0e19a42ffc93c21120626883d3b5b445bff2
TheSilenceOfMind <baladkirill@mail.ru>
2018-07-05 20:41:01 +0300

    Add controller for handling request related to user of social network.

---
+++ spitter/src/main/java/org/practice/spitter/web/SpitterController.java
@@ -0,0 +1,17 @@
+package org.practice.spitter.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/spitter")
+public class SpitterController {
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegistrationForm() {
+        return "registerForm";
+    }
+
+}
