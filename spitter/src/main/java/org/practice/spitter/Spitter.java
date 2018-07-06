d91978e433869e91727367c1116fda9d17c4d89a
TheSilenceOfMind <baladkirill@mail.ru>
2018-07-07 01:12:40 +0300

    Improve validation using localisation feature to error messages.

--- spitter/src/main/java/org/practice/spitter/Spitter.java
+++ spitter/src/main/java/org/practice/spitter/Spitter.java
@@ -2,6 +2,7 @@ package org.practice.spitter;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -14,19 +15,19 @@ public class Spitter {
     private Long id;
 
     @NotNull
-    @Size(min = 5, max = 16)
+    @Size(min = 5, max = 16, message = "{username.size}")
     private String username;
 
     @NotNull
-    @Size(min = 5, max = 25)
+    @Size(min = 5, max = 25, message = "{password.size}")
     private String password;
 
     @NotNull
-    @Size(min = 2, max = 30)
+    @Size(min = 2, max = 30, message = "{firstName.size}")
     private String firstName;
 
     @NotNull
-    @Size(min = 2, max = 30)
+    @Size(min = 2, max = 30, message = "{lastName.size}")
     private String lastName;
 
     public Spitter() {
--- spitter/src/main/java/org/practice/spitter/config/WebConfig.java
+++ spitter/src/main/java/org/practice/spitter/config/WebConfig.java
@@ -1,13 +1,19 @@
 package org.practice.spitter.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -28,4 +34,20 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         configurer.enable();
     }
 
+    // used to set resources location
+    @Bean
+    public MessageSource messageSource() {
+//        Locale.setDefault(new Locale("ru")); // to test russian property localisation
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("registrationValidationMessages");
+        return source;
+    }
+
+    // used to set resources location
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(messageSource());
+        return validator;
+    }
 }
