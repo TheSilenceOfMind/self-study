aff056dd60fb7782bbcbafe788f5f4df5f4c04f9
TheSilenceOfMind <baladkirill@mail.ru>
2018-11-10 00:15:14 +0300

    upload Hibernate example from mkyong

---
+++ HibernateExample/src/main/java/com/mkyong/util/HibernateUtil.java
@@ -0,0 +1,28 @@
+package com.mkyong.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // create session factory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // log the exception
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
